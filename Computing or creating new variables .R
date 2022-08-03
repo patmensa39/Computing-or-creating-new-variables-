dd48a83c4166a5b83c37eaef3fb191c0048df3c2
Patrick Mensah <70266683+patmensa39@users.noreply.github.com>
2022-08-03 00:03:36 -0400

    Add files via upload

---
+++ Computing or creating new variables .R
@@ -0,0 +1,30 @@
+#COMPUTING OR CREATING NEW VARIABLES ###
+### Loading the libraries ###
+pacman::p_load(pacman, rio, tidyverse)
+
+### loading and preparing the dataset (Google search data) 
+data<- import("StateData.xlsx") %>% as_tibble() %>% select(state_code, 
+          psychRegions, instagram:modernDance) %>% 
+        mutate(psychRegions = as.factor(psychRegions))
+
+view(data) # viewing the data 
+
+### Creating new variables as a result of the averages of old variables 
+
+newdata<- data %>% mutate(Philant = (entrepreneur + gdpr + museum + university) / 3,
+                          Patrick = (volunteering + museum + scrapbook + modernDance) / 4) %>% 
+               select(state_code,psychRegions, Philant, Patrick)
+view(newdata)
+
+
+### Doing a reverse coding 
+
+newdata2 <- data %>%  mutate(Houseowner = (university + (mortgage * -1)) / 2 )%>% 
+           select(state_code, Houseowner, university, mortgage)
+view(newdata2)  
+
+### There are two package that is more convenient in working with reverse codeing 
+# install.packages("psych")
+# install.packages("scales")  
+  
+  
